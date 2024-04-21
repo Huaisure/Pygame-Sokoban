b9348dff7ae457ee0fd83404668d074dfa8b4e42
Huaisure <hs-liu21@mails.tsinghua.edu.cn>
2024-04-21 19:04:48 +0800

    完善对话

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ import time
 
 def main():
     # 读取任务和关卡号
+    print("欢迎来到推箱子游戏！")
+    print(
+        "*任务1：若箱子被推到洞口后不会消失（可以再次被推出来），且箱子和洞口不存在一一对应的关系，找到最优的移动路线"
+    )
+    print("*任务2：若箱子被推到洞口后会消失，且箱子和洞口一一对应，找到最优的移动路线")
     task = int(input("完成任务1 or 任务2(请输入1或2)："))
     level = int(input("请输入关卡号（1-10）："))
     assert level in range(1, 11), "关卡号输入错误"
 
-    if_random = int(input("是否随机生成地图（1:随机 or 0:固定）："))
+    if_random = int(input("是否随机生成地图（0:固定 or 1:随机）："))
 
     # 生成游戏类
     my_game = Game(level=level, size=(800, 600), task=task, if_random=if_random)
